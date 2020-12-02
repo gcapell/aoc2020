1f0d27fc6143c7b93f0459d81874a09ac921327e
Gary Capell <gary.capell@gmail.com>
2020-12-02 21:41:32 +1100

    tidying

--- src/day2.rs
+++ src/day2.rs
@@ -1,26 +1,32 @@
 use std::fs;
 use std::io::{self, BufRead};
 
-pub fn count_matches(validate: fn(usize, usize, &str, &str) -> bool) -> i32 {
-    let f = fs::File::open("input.txt").unwrap();
-    let lines = io::BufReader::new(f).lines();
-    let mut count = 0;
-    for line in lines {
-        if let Ok(p) = line {
-            if let [counts, needle, haystack] =
-                p.split_ascii_whitespace().collect::<Vec<&str>>().as_slice()
-            {
-                if let [min, max] = counts.split("-").collect::<Vec<&str>>().as_slice() {
-                    let min = min.parse::<usize>().unwrap();
-                    let max = max.parse::<usize>().unwrap();
-                    if validate(min, max, &needle[..1], haystack) {
-                        count += 1;
-                    }
-                }
-            }
+pub fn count_matches(validate: fn(usize, usize, &str, &str) -> bool) -> usize {
+    io::BufReader::new(fs::File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|x| {
+            let (min, max, needle, haystack) = parse(x);
+            validate(min, max, &needle[..1], haystack)
+        })
+        .count()
+}
+
+fn parse(line: &str) -> (usize, usize, &str, &str) {
+    if let [counts, needle, haystack] = line
+        .split_ascii_whitespace()
+        .collect::<Vec<&str>>()
+        .as_slice()
+    {
+        if let [min, max] = counts.split("-").collect::<Vec<&str>>().as_slice() {
+            return (atou(min), atou(max), &needle[..1], haystack);
         }
     }
-    count
+    panic!("bad line: {}", line);
+}
+
+fn atou(s: &str) -> usize {
+    s.parse::<usize>().unwrap()
 }
 
 pub fn policy1(min: usize, max: usize, needle: &str, haystack: &str) -> bool {
