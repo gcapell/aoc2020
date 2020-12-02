9ae3eddc933e888b9db56b95cf8b45ba8c049eb1
Gary Capell <gary.capell@gmail.com>
2020-12-02 16:35:34 +1100

    day1

---
+++ src/day1.rs
@@ -0,0 +1,14 @@
+use crate::util;
+use std::fs;
+use std::io::Error;
+
+pub fn first(size: usize) -> Result<(), Error> {
+    let mut v = util::ints_from(fs::File::open("input.txt")?)?;
+    v.sort();
+
+    let mut answer = vec![0; size];
+    if util::summing_elements(&v, size, 2020, &mut answer) {
+        println!("{:?}-> {}", answer, answer.iter().fold(1, |acc,x| acc*x));
+    }
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use std::io::Error;
+mod day1;
+mod util;
+
+fn main() -> Result<(), Error> {
+    day1::first(3)
+}
