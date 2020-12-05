cc5f466bd579d146bbe4dff4cf8a1da45eb4e691
Gary Capell <gary.capell@gmail.com>
2020-12-05 22:12:44 +1100

    incorporate suggestions/inspirationsfrom r/adventofcode, especially u/Valdron

--- src/day5.rs
+++ src/day5.rs
@@ -6,29 +6,21 @@ pub fn highest_seat_id() {
     let mut ids: Vec<i32> = io::BufReader::new(fs::File::open("input.txt").unwrap())
         .lines()
         .filter_map(Result::ok)
-        .map(|line| id(&line))
+        .map(binary)
         .collect();
     ids.sort();
     println!(
-        "missing{:?}",
+        "missing: {:?}",
         &ids.windows(2)
-            .find(|x| {
-                if let &&[a, b] = x {
-                    a + 1 != b
-                } else {
-                    panic!()
-                }
+            .find_map(|w| match w {
+                &[a,b] if a+1 != b => Some(a+1),
+                _ => None,
             })
-            .unwrap()
     );
 }
 
-fn id(s: &str) -> i32 {
-    binary(&s[..7], 'B') * 8 + binary(&s[7..], 'R')
-}
-
-fn binary(s: &str, one: char) -> i32 {
+fn binary(s: String) -> i32 {
     s.chars()
-        .map(|c| if c == one { 1 } else { 0 })
+        .map(|c|  (c == 'B' || c == 'R') as i32 )
         .fold(0, |t, b| t * 2 + b)
 }
