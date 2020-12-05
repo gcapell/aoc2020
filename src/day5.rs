fde3f656f90fe7371943ef97cc28b63682f23c06
Gary Capell <gary.capell@gmail.com>
2020-12-05 16:44:48 +1100

    simplified ID calculation

--- src/day5.rs
+++ src/day5.rs
@@ -6,28 +6,14 @@ pub fn highest_seat_id() {
     let x = io::BufReader::new(fs::File::open("input.txt").unwrap())
         .lines()
         .filter_map(Result::ok)
-        .map(|s| Seat::new(&s).id())
+        .map(|line|id(&line))
         .max()
         .unwrap();
     println!("x:{}", x);
 }
 
-#[derive(Debug)]
-struct Seat {
-    row: i32,
-    col: i32,
-}
-
-impl Seat {
-    fn new(s: &str) -> Seat {
-        Seat {
-            row: binary(&s[..7], 'B'),
-            col: binary(&s[7..], 'R'),
-        }
-    }
-    fn id(&self) -> i32 {
-        self.row * 8 + self.col
-    }
+fn id(s: &str) -> i32 {
+    binary(&s[..7], 'B') * 8 + binary(&s[7..], 'R')
 }
 
 fn binary(s: &str, one: char) -> i32 {
