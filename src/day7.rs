66866f887391b5dc8cc46bb485e1822d2fd9898f
Gary Capell <gary.capell@gmail.com>
2020-12-07 23:00:20 +1100

    day7 part2

--- src/day7.rs
+++ src/day7.rs
@@ -1,6 +1,7 @@
 use daggy;
+use daggy::Walker;
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::io;
 use std::io::BufRead;
@@ -8,10 +9,10 @@ use std::io::BufRead;
 pub fn dot() {
     let re = Regex::new(r"(\d+) (.*) bags?\.?").unwrap();
     let mut dag = daggy::Dag::<u32, usize, u32>::new();
-    let mut node_names = HashMap::new();
-    let mut name_nodes = HashMap::new();
+    let mut node_names: HashMap<String, usize> = HashMap::new();
+    let mut name_nodes: HashMap<usize, String> = HashMap::new();
 
-    for line in io::BufReader::new(fs::File::open("demo.txt").unwrap())
+    for line in io::BufReader::new(fs::File::open("input.txt").unwrap())
         .lines()
         .filter_map(Result::ok)
     {
@@ -19,7 +20,7 @@ pub fn dot() {
             let outer = outer.strip_suffix(" bags").unwrap();
             let outer_id = find_or_add(outer, &mut dag, &mut node_names, &mut name_nodes);
             if *inner == "no other bags." {
-                continue
+                continue;
             }
             for x in inner.split(", ") {
                 let mat = re.captures(x).unwrap();
@@ -31,20 +32,66 @@ pub fn dot() {
                     count,
                 )
                 .unwrap();
-                
             }
         }
     }
-    graphviz(&dag, &name_nodes);
+    if false {
+        graphviz(&dag, &name_nodes);
+    }
+    let start = daggy::NodeIndex::new(*(node_names.get("shiny gold").unwrap()));
+    let mut visited = HashSet::new();
+    println!(
+        "ancestors:{}",
+        count_distinct_ancestors(&dag, start, &mut visited)
+    );
+    let mut cache = HashMap::new();
+    println!("contents:{}", contents(&dag, start, &mut cache));
 }
 
-fn graphviz(dag: &daggy::Dag<u32, usize, u32>, nodes: &HashMap<usize,String>){
+fn contents(
+    dag: &daggy::Dag<u32, usize, u32>,
+    start: daggy::NodeIndex,
+    cache: &mut HashMap<daggy::NodeIndex, u32>,
+) -> u32 {
+    if let Some(n) = cache.get(&start) {
+        return *n;
+    }
+    let mut count: u32 = 0;
+    for (e, c) in dag.children(start).iter(dag) {
+        let w = *dag.edge_weight(e).unwrap() as u32;
+        count += w * (1 + contents(dag, c, cache));
+    }
+    count
+}
+
+fn count_distinct_ancestors(
+    dag: &daggy::Dag<u32, usize, u32>,
+    start: daggy::NodeIndex,
+    visited: &mut HashSet<daggy::NodeIndex>,
+) -> u32 {
+    let mut count = 0;
+    for (_, p) in dag.parents(start).iter(dag) {
+        if visited.contains(&p) {
+            continue;
+        }
+        visited.insert(p);
+        count += 1 + count_distinct_ancestors(dag, p, visited);
+    }
+    count
+}
+
+fn graphviz(dag: &daggy::Dag<u32, usize, u32>, nodes: &HashMap<usize, String>) {
     println!("digraph G {{");
-    for e in 0..dag.edge_count(){
+    for e in 0..dag.edge_count() {
         let e = daggy::EdgeIndex::new(e);
         let w = dag.edge_weight(e).unwrap();
-        let (a,b) = dag.edge_endpoints(e).unwrap();
-        println!(r#"   "{}"-> "{}" [ label = "{}"] "#, nodes.get(&a.index()).unwrap(), nodes.get(&b.index()).unwrap(), w);
+        let (a, b) = dag.edge_endpoints(e).unwrap();
+        println!(
+            r#"   "{}"-> "{}" [ label = "{}"] "#,
+            nodes.get(&a.index()).unwrap(),
+            nodes.get(&b.index()).unwrap(),
+            w
+        );
     }
     println!("}}");
 }
