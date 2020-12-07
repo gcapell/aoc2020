e77b2376d3ca92960f0d5dc556576dfcfb97d079
Gary Capell <gary.capell@gmail.com>
2020-12-07 22:23:45 +1100

    Generate graphviz from daggy

--- src/day7.rs
+++ src/day7.rs
@@ -7,9 +7,9 @@ use std::io::BufRead;
 
 pub fn dot() {
     let re = Regex::new(r"(\d+) (.*) bags?\.?").unwrap();
-    println!("digraph G {{");
     let mut dag = daggy::Dag::<u32, usize, u32>::new();
     let mut node_names = HashMap::new();
+    let mut name_nodes = HashMap::new();
 
     for line in io::BufReader::new(fs::File::open("demo.txt").unwrap())
         .lines()
@@ -17,42 +17,50 @@ pub fn dot() {
     {
         if let [outer, inner] = line.split(" contain ").collect::<Vec<&str>>().as_slice() {
             let outer = outer.strip_suffix(" bags").unwrap();
-            let outer_id = find_or_add(outer, &mut dag, &mut node_names);
+            let outer_id = find_or_add(outer, &mut dag, &mut node_names, &mut name_nodes);
+            if *inner == "no other bags." {
+                continue
+            }
             for x in inner.split(", ") {
-                if x == "no other bags." {
-                    continue;
-                }
-
                 let mat = re.captures(x).unwrap();
-                let (count, name) = (&mat[1], &mat[2]);
-                let count = count.parse::<usize>().unwrap();
-                let inner_id = find_or_add(name, &mut dag, &mut node_names);
+                let (count, name) = (mat[1].parse::<usize>().unwrap(), &mat[2]);
+                let inner_id = find_or_add(name, &mut dag, &mut node_names, &mut name_nodes);
                 dag.add_edge(
                     daggy::NodeIndex::new(outer_id),
                     daggy::NodeIndex::new(inner_id),
                     count,
                 )
                 .unwrap();
-
-                println!(r#"   "{}"-> "{}" [ label = "{}"] "#, outer, name, count);
+                
             }
         }
     }
+    graphviz(&dag, &name_nodes);
+}
+
+fn graphviz(dag: &daggy::Dag<u32, usize, u32>, nodes: &HashMap<usize,String>){
+    println!("digraph G {{");
+    for e in 0..dag.edge_count(){
+        let e = daggy::EdgeIndex::new(e);
+        let w = dag.edge_weight(e).unwrap();
+        let (a,b) = dag.edge_endpoints(e).unwrap();
+        println!(r#"   "{}"-> "{}" [ label = "{}"] "#, nodes.get(&a.index()).unwrap(), nodes.get(&b.index()).unwrap(), w);
+    }
     println!("}}");
-    println!("names: {:?}", node_names);
-    println!("names: {:?}", dag);
 }
 
 fn find_or_add(
     name: &str,
     dag: &mut daggy::Dag<u32, usize, u32>,
     names: &mut HashMap<String, usize>,
+    nodes: &mut HashMap<usize, String>,
 ) -> usize {
     if let Some(o) = names.get(name) {
         *o
     } else {
         let i = dag.add_node(0).index();
         names.insert(name.to_string(), i);
+        nodes.insert(i, name.to_string());
         i
     }
 }
