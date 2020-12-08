c16e1b8ce7e9d7c01160c38c12370429a4fa3c44
Gary Capell <gary.capell@gmail.com>
2020-12-08 22:21:01 +1100

    day8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -9,12 +9,18 @@ pub fn run() {
         .map(Instruction::parse)
         .collect();
 
-    println!("{}", Machine::new().run_till_loop(instructions));
+    let mut i_swapped = vec![false; instructions.len()];
+    loop {
+        if let Some(n) = Machine::new().run_till_loop_or_end(&instructions, &mut i_swapped) {
+            println!("{}", n);
+            break;
+        }
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Instruction {
-    Nop,
+    Nop(i32),
     Acc(i32),
     Jmp(i32),
 }
@@ -26,10 +32,17 @@ impl Instruction {
         let sign = if s.as_bytes()[4] == b'-' { -1 } else { 1 };
         let operand = &s[5..].parse::<i32>().unwrap() * sign;
         match code {
-            "nop" => Nop,
+            "nop" => Nop(operand),
             "acc" => Acc(operand),
             "jmp" => Jmp(operand),
             _ => panic!(s),
+        }
+    }
+    fn swap(self: Instruction) -> Option<Instruction> {
+        match self {
+            Nop(n) => Some(Jmp(n)),
+            Acc(_) => None,
+            Jmp(n) => Some(Nop(n)),
         }
     }
 }
@@ -44,15 +57,32 @@ impl Machine {
     fn new() -> Machine {
         Machine { pc: 0, acc: 0 }
     }
-    fn run_till_loop(self: &mut Machine, i: Vec<Instruction>) -> i32 {
-        let mut executed = vec![false; i.len()];
+    fn run_till_loop_or_end(
+        self: &mut Machine,
+        instructions: &Vec<Instruction>,
+        i_swapped: &mut Vec<bool>,
+    ) -> Option<i32> {
+        let mut executed = vec![false; instructions.len()];
+        let mut swapped = false;
+
         loop {
+            if self.pc == instructions.len() {
+                return Some(self.acc);
+            }
             if executed[self.pc] {
-                return self.acc;
+                return None;
+            }
+            let mut i = instructions[self.pc];
+            if !swapped && !i_swapped[self.pc] {
+                if let Some(s) = i.swap() {
+                    i = s;
+                    swapped = true;
+                    i_swapped[self.pc] = true;
+                }
             }
             let prev = self.pc;
-            match i[self.pc] {
-                Nop => {
+            match i {
+                Nop(_) => {
                     self.pc += 1;
                 }
                 Acc(n) => {
