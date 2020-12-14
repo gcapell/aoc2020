87772a92b9e43b58a287c5e4f276744ff437882e
Gary Capell <gary.capell@gmail.com>
2020-12-14 20:40:42 +1100

    14/2

--- src/day14.rs
+++ src/day14.rs
@@ -5,8 +5,9 @@ use std::io::BufRead;
 
 pub fn run() {
     let mut fixed = 0;
-    let mut mask = 0;
     let mut mem = HashMap::new();
+    let mut floats = Vec::new();
+    let mut z = 0;
     for line in io::BufReader::new(fs::File::open("input.txt").unwrap())
         .lines()
         .filter_map(Result::ok)
@@ -14,37 +15,57 @@ pub fn run() {
         let chunks: Vec<&str> = line.split(" = ").collect();
         match chunks.as_slice() {
             ["mask", m] => {
-                let (f, m) = parse_mask(m);
-                fixed = f;
-                mask = m;
+                let (fi, fl) = parse_mask(m);
+                fixed = fi;
+                floats = fl;
+                z = zeroes(&floats);
             }
-            [pos, val] => {
-                let pos = parse_pos(pos);
-                let src = val.parse::<i64>().unwrap();
-                mem.insert(pos, fixed | (src & mask));
+            [addr, val] => {
+                let addr = parse_addr(addr);
+                let val = val.parse::<i64>().unwrap();
+                let base = (addr | fixed) & !z;
+                for m in 0..(1 << floats.len()) {
+                    mem.insert(base | float_mask(m, &floats), val);
+                }
             }
             _ => panic!(),
         }
     }
-    println!("map: {:?}", mem);
     println!("{}", mem.values().sum::<i64>())
 }
 
-fn parse_mask(m: &str) -> (i64, i64) {
+fn float_mask(m: usize, floats: &[usize]) -> i64 {
+    let mut reply = 0;
+    for (i, p) in floats.iter().enumerate() {
+        if m & (1 << i) != 0 {
+            reply |= 1 << p;
+        }
+    }
+    reply
+}
+
+fn zeroes(floats: &Vec<usize>) -> i64 {
+    let mut z = 0;
+    for u in floats {
+        z |= 1 << u;
+    }
+    z
+}
+
+fn parse_mask(m: &str) -> (i64, Vec<usize>) {
     let mut fixed = 0;
-    let mut mask = 0;
+    let mut float = Vec::new();
     for (p, c) in m.trim().chars().rev().enumerate() {
         match c {
             '0' => (),
             '1' => fixed |= 1 << p,
-            'X' => mask |= 1 << p,
+            'X' => float.push(p),
             _ => panic!(),
         }
     }
-    println!("parse_mask({})->fixed:{:b}, mask:{:b}", m, fixed, mask);
-    (fixed, mask)
+    (fixed, float)
 }
 
-fn parse_pos(m: &str) -> i64 {
+fn parse_addr(m: &str) -> i64 {
     m[4..m.len() - 1].parse().unwrap()
 }
