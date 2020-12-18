d5a3573fcf7922ee4e04d5aa2de0019807fd7299
Gary Capell <gary.capell@gmail.com>
2020-12-18 21:57:16 +1100

    18/2

--- src/day18.rs
+++ src/day18.rs
@@ -13,54 +13,71 @@ pub fn run() {
     );
 }
 
-#[derive(Debug)]
-struct Acc {
-    n: Option<i64>,
-    op: char,
+struct Lex<'a> {
+    pos: usize,
+    s: &'a [u8],
 }
 
-impl Acc {
-    fn new() -> Acc {
-        Acc { n: None, op: ' ' }
+impl<'a> Lex<'a> {
+    fn expect(&mut self, c: u8) -> bool {
+        self.ws();
+        if self.pos == self.s.len() {
+            return false;
+        }
+        if self.s[self.pos] == c {
+            self.pos += 1;
+            true
+        } else {
+            false
+        }
     }
 
-    fn merge(&mut self, v: i64) {
-        self.n = if let Some(n) = self.n {
-            Some(match self.op {
-                '*' => v * n,
-                '+' => v + n,
-                _ => panic!(),
-            })
-        } else {
-            Some(v)
+    fn pop(&mut self) -> u8 {
+        self.ws();
+        let c = self.s[self.pos];
+        self.pos += 1;
+        c
+    }
+
+    fn ws(&mut self) {
+        while self.pos < self.s.len() && self.s[self.pos] == b' ' {
+            self.pos += 1;
         }
     }
 }
 
 fn eval(line: String) -> i64 {
-    let mut acc = Acc::new();
-    let mut stack = Vec::new();
-    for c in line.chars() {
-        match c {
-            ' ' => (),
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                acc.merge(c as i64 - '0' as i64)
-            }
-            '(' => {
-                stack.push(acc);
-                acc = Acc::new();
-            }
-            ')' => {
-                let p_acc = acc;
-                acc = stack.pop().unwrap();
-                acc.merge(p_acc.n.unwrap());
-            }
-            '*' | '+' => acc.op = c,
-            _ => panic!(),
-        }
-        //println!("after {}, acc:{:?}, stack:{:?}", c, acc, stack);
-    }
-    let n = acc.n.unwrap();
+    let n = expr(&mut Lex {
+        pos: 0,
+        s: line.as_bytes(),
+    });
     println!("{} = {}", line, n);
     n
+}
+
+fn expr(lex: &mut Lex) -> i64 {
+    let mut acc = sum(lex);
+    while lex.expect(b'*') {
+        acc *= sum(lex);
+    }
+    acc
+}
+
+fn sum(lex: &mut Lex) -> i64 {
+    let mut acc = term(lex);
+    while lex.expect(b'+') {
+        acc += term(lex);
+    }
+    acc
+}
+
+fn term(lex: &mut Lex) -> i64 {
+    let c = lex.pop();
+    if c == b'(' {
+        let n = expr(lex);
+        lex.expect(b')');
+        n
+    } else {
+        (c - b'0').into()
+    }
 }
