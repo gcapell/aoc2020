7b2ee1ee617d2055975120e2fa1bfc29d74e38f3
Gary Capell <gary.capell@gmail.com>
2020-12-23 17:33:01 +1100

    no need for l pointers

--- day23.go
+++ day23.go
@@ -3,35 +3,21 @@ package main
 import "fmt"
 
 const maxCup = 1_000_000
+const rounds = 10_000_000
 
 type Cup struct {
-	n    int
-	l, r *Cup
+	n int
+	r *Cup
 }
 
 func main() {
-	fmt.Println("hello")
-
 	c, l2c := cups("253149867")
 
-	for j := 0; j < 10_000_000; j++ {
+	for j := 0; j < rounds; j++ {
 		c = round(c, l2c)
-		//printCircle(c)
 	}
 	cup1 := l2c[1]
 	fmt.Println(cup1.r.n * cup1.r.r.n)
-}
-
-func printCircle(h *Cup) {
-	c := h
-	for {
-		fmt.Printf("%d,", c.n)
-		c = c.r
-		if c == h {
-			fmt.Println()
-			return
-		}
-	}
 }
 
 func round(c *Cup, labelToCup []*Cup) *Cup {
@@ -39,14 +25,11 @@ func round(c *Cup, labelToCup []*Cup) *Cup {
 	h := c.r
 	t := c.r.r.r
 	c.r = t.r
-	t.r.l = c
 
 	dst := labelToCup[findLabel(c.n, h)]
 
 	// insert
 	t.r = dst.r
-	h.l = dst
-	dst.r.l = t
 	dst.r = h
 
 	return c.r
@@ -56,7 +39,7 @@ func findLabel(n int, h *Cup) int {
 	for {
 		n--
 		if n < 1 {
-			n = 1_000_000
+			n = maxCup
 		}
 		if !(n == h.n || n == h.r.n || n == h.r.r.n) {
 			return n
@@ -75,13 +58,10 @@ func cups(s string) (*Cup, []*Cup) {
 		if head == nil {
 			head = c
 			tail = c
-			c.l = c
 			c.r = c
 		} else {
 			tail.r = c
-			c.l = tail
 			c.r = head
-			head.l = c
 			tail = c
 		}
 	}
@@ -89,9 +69,7 @@ func cups(s string) (*Cup, []*Cup) {
 		c := &Cup{n: j}
 		l2c[j] = c
 		tail.r = c
-		c.l = tail
 		c.r = head
-		head.l = c
 		tail = c
 	}
 	return head, l2c
